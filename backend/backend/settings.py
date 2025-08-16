42c67415882dcd9008d72e8ee441670852adeb28
Kevin Thulnith <112649346+kevinThulnith@users.noreply.github.com>
2025-08-17 03:24:14 +0530

    2nd commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,12 +130,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#     )
+# }
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.{}".format(
+            os.getenv("DATABASE_ENGINE", "sqlite3")
+        ),
+        "NAME": os.getenv("DATABASE_NAME", "polls"),
+        "USER": os.getenv("DATABASE_USERNAME", "myprojectuser"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
+        "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
+        "PORT": os.getenv("DATABASE_PORT", 5432),
+    }
 }
 
 # Password validation
---
+++ backend/entrypoint.prod.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python manage.py collectstatic --noinput
+python manage.py migrate --noinput
+daphne -b 0.0.0.0 -p 8000 backend.asgi:application
